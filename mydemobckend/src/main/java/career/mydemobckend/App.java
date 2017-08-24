0e929086fdf1672c8e5df8e47626d128e20603e6
madhurishedge <madhuri.shedage@gmail.com>
2017-08-24 07:36:07 -0700

    part 1 commiting

---
+++ mydemobckend/src/main/java/career/mydemobckend/App.java
@@ -0,0 +1,13 @@
+package career.mydemobckend;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
